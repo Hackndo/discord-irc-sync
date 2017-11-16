924a34ced546a7c38a4e492c21db3e7bea8816b1
Newbiecontest <newbiecontest@gmail.com>
2017-11-16 20:00:39 +0100

    Possibility to choose config file

--- discord-irc-sync.py
+++ discord-irc-sync.py
@@ -2,11 +2,20 @@
 
 import json
 import os
+import sys
 
 from src.ircclient import IRCClient
 from src.discordclient import DiscordClient
 
-with open(os.path.join("config", "config.json"), encoding="utf-8") as f:
+config_file = os.path.join("config", "config.json")
+
+if len(sys.argv) == 2:
+    config_file = sys.argv[1]
+
+if not os.path.isfile(config_file):
+    sys.exit("File %s doesn't exist" % config_file)
+
+with open(config_file, encoding="utf-8") as f:
     settings = json.loads(f.read())
 
 discord_client = DiscordClient(settings)
@@ -16,5 +25,6 @@ discord_client.set_irc(irc_client)
 irc_client.set_discord(discord_client)
 
 
+irc_client.start()
 irc_client.h_run()
 discord_client.h_run()
