b2e6d37b236847dc67779461e8e2412df8c27d93
hackndo <hackndo@gmail.com>
2017-11-17 14:16:35 +0100

    Add IRC ssl

--- src/ircclient.py
+++ src/ircclient.py
@@ -14,6 +14,7 @@ class IRCClient(irc.bot.SingleServerIRCBot):
     def __init__(self, configuration):
         self.h_server = configuration['irc']["server"]
         self.h_port = int(configuration['irc']["port"])
+        self.h_ssl = configuration['irc']['ssl']
         self.h_nickname = configuration['irc']["nickname"]
         self.h_channel = configuration['irc']["channel"]
         self.h_owner = configuration['irc']["owner"]
@@ -25,7 +26,11 @@ class IRCClient(irc.bot.SingleServerIRCBot):
         self.h_discord = None
         self.h_connection = None
 
-        super().__init__([(self.h_server, self.h_port)], self.h_nickname, self.h_nickname)
+        if self.h_ssl:
+            ssl_factory = irc.connection.Factory(wrapper=ssl.wrap_socket)
+            super().__init__([(self.h_server, self.h_port)], self.h_nickname, self.h_nickname, connect_factory=ssl_factory)
+        else:
+            super().__init__([(self.h_server, self.h_port)], self.h_nickname, self.h_nickname)
 
     def set_discord(self, discord):
         self.h_discord = discord
